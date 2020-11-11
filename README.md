adc3e2ffce5bc48a3cf6bc880a503a1d547ae786
jaws0227 <jaws0910@gmail.com>
2020-11-11 15:32:57 +0900

    gradle build DSl groovy -> kotlin 변경

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # 코틀린을 이용한 Spring boot 연습
 코틀린 + spring boot2를 이용한 Rest Api 서버 구성 연습
 
+## Gradle
+Gradle 5.0 이후 `Gradle Kotlin DSL`을 지원한다.
+Kotlin 프로젝트인지 Gradle 프로젝트인지는 확장자로 구분한다. 
+* Groovy DSL: .gradle -> build.gradle, settings.gradle
+* Kotlin DSL: .gradle.kts -> build.gradle.kts, settings.gradle.kts
+
+기존 groovy를 사용했을 때는 Intellij Community에서 자동완성을 지원하지 않았는데 Kotlin은 지원해준다.
+하지만.. 그렇다고 그렇게 좋게 지원하지는 않는다...
+
 ## 참고자료 
-* https://daddyprogrammer.org/post/19/spring-boot2-start-intellij/
+* [SpringBoot2로 Rest api 만들기](https://daddyprogrammer.org/post/19/spring-boot2-start-intellij/)
+* ['Gradle Kotlin DSL' 이야기](https://woowabros.github.io/tools/2019/04/30/gradle-kotlin-dsl.html)
---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.3.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    kotlin("jvm") version "1.3.72"
+    kotlin("plugin.spring") version "1.3.72"
+    kotlin("plugin.jpa") version "1.3.72"
+}
+
+group = "me.sangoh.traning"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
